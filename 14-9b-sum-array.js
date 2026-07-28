d45f74f6aa2b65a19852bb576409ae0e80992cd7
Aminul islam <aminulislam424842@gmail.com>
2026-07-28 14:07:26 +0600

    fix: correct loop start index in sum array function

---
+++ 14-9b-sum-array.js
@@ -0,0 +1,11 @@
+function sumArray(numbers) {
+  let total = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+  }
+
+  return total;
+}
+
+console.log(sumArray([1, 2, 3, 4]))
