865eacbe775474553062ca7292bbe7579984f19f
Aminul islam <aminulislam424842@gmail.com>
2026-07-28 13:16:21 +0600

    fix: identify and fix syntax, runtime, and logic errors

---
+++ 14-1-spot-error-type.js
@@ -0,0 +1,22 @@
+// Snippet A
+
+function sayHi(name) {
+  console.log("Hi, " + name )
+}
+
+sayHi("Mira");
+
+
+// Snippet B
+
+let word = "hello";
+console.log(word.toUpperCase());
+
+
+// Snippet C
+
+function square(num) {
+  return num *= num;
+}
+
+console.log(square(4)); 
